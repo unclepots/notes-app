26c5994e222e1721bf20143fd6dd4343b5c0f110
Anatoly Yusufov <anatoly.yusufov@gmail.com>
2018-09-24 23:51:00 -0400

    Request Callback fix

--- config/keys.js
+++ config/keys.js
@@ -3,17 +3,19 @@ require('dotenv').config();
 let db_name = process.env.DB_NAME || 'production';
 let is_localhost = process.env.PORT === 4000 ? true : false;
 
+let domain = process.env.PORT === 4000 ? 'https://localhost:4000' : 'https://anatoly-notes-app.herokuapp.com';
+
 module.exports = {
     is_localhost: is_localhost,
     google: {
         client_id: process.env.GOOGLE_ID,
         client_secret: process.env.GOOGLE_SECRET,
-        callback: "https://localhost:4000/auth/google/redirect"
+        callback: "/auth/google/redirect"
     },
     facebook: {
         client_id: process.env.FACEBOOK_ID,
         client_secret: process.env.FACEBOOK_SECRET,
-        callback: "http://localhost:4000/auth/facebook/redirect"
+        callback: "/auth/facebook/redirect"
     },
     session: {
         cookieKey: process.env.COOKIE_KEY
